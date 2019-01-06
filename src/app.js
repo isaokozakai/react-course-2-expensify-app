5da04eaba075564e1e43fd0f8c3e9c93b7d00720
Isao <feat130@hotmail.co.jp>
2019-01-06 13:26:58 +0900

    Add setExpenses

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter, sortByAmount } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses'
 import configureStore from './store/configureStore';
@@ -23,4 +23,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -9,19 +9,21 @@ export default (state = expensesReducerDefaultState, action) => {
         ...state,
         action.expense
       ];
-      case 'REMOVE_EXPENSE':
-        return state.filter(({ id }) => id !== action.id);
-      case 'EDIT_EXPENSE':
-        return state.map((expense) => {
-          if (expense.id === action.id) {
-            return {
-              ...expense,
-              ...action.updates
-            };
-          } else {
-            return expense;
-          }
-        });
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+           ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      });
+    case 'SET_EXPENSES':
+      return action.expenses;
     default:
       return state;
   }
